2d23c777c81c441d10e60b7ec7c34bd4b813c526
Rishabh Gupta <rishabh9aug@gmail.com>
2024-05-19 14:07:48 +0530

    here also we can see how daemon thread got terminated also learnt how same varibale name from parent class is accesed via super keyword ,,see notio OOPs page made in this doc to understand in depth

--- src/Thread1.java
+++ src/Thread1.java
@@ -2,6 +2,12 @@
 
 public class Thread1 extends Thread
 {
+
+    public Thread1(String threadName)
+    {
+        super(threadName);
+    }
+
     @Override
     public void run()
     {
@@ -9,7 +15,7 @@ public class Thread1 extends Thread
         int n=10;
         for(int i=0;i<n;i++)
         {
-            System.out.println("inside thread1"+i);
+            System.out.println("inside "+Thread.currentThread().getName()+ "  "+i);
         }
     }
 }
--- src/ThreadTester.java
+++ src/ThreadTester.java
@@ -5,7 +5,8 @@ public class ThreadTester {
     public static void main(String[] args)
     {
         System.out.println("Main is starting");
-        Thread thread1=new Thread1();
+        Thread thread1=new Thread1("Thread1");
+        thread1.setDaemon(true);
         thread1.start();
         System.out.println("Main is exiting");
 
