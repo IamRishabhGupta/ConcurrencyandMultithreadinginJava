87a747435195c5656359bfb7cdebe134c6862d48
Rishabh Gupta <rishabh9aug@gmail.com>
2024-06-03 22:20:27 +0530

    stack class imlimented with thread sleep

---
+++ src/stack.java
@@ -0,0 +1,58 @@
+public class stack {
+    
+
+
+    private int[] array;
+    private int stacktop;
+    public stack(int capacity)
+    {
+        array=new int[capacity];
+        stacktop=-1;
+    }
+
+    public boolean isEmpty()
+    {
+        return stacktop==-1;
+    }
+    public boolean isFull()
+    {
+        return stacktop>=array.length-1;
+    }
+    public boolean push(int value)
+    {
+        if(isFull())
+        {
+            return false;
+        }
+        stacktop++;
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        array[stacktop]=value;
+        return true;
+    }
+
+    public int pop()
+    {
+        if(isEmpty())
+        {
+            return Integer.MIN_VALUE;
+        }
+        int obj=array[stacktop];
+        array[stacktop]=Integer.MIN_VALUE;
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        stacktop--;
+        return obj;
+    }
+
+}
