a92825ebb9bba92967342d06f70424f665449462
Rishabh Gupta <rishabh9aug@gmail.com>
2024-06-03 23:19:02 +0530

    now here what we done is used a lock and note that we have used the very same lock for both so let suppose there are multiple threads t1 t2 etc now if t1 lock lekr baith gya and meanwhile t2 tried doing something it wont be able to because lock is with t1 so concurrency issue solved

--- src/stack.java
+++ src/stack.java
@@ -4,15 +4,17 @@ public class Stack {
 
     private int[] array;
     private int stacktop;
+    Object lock=new Object();
     public Stack(int capacity)
     {
         array=new int[capacity];
         stacktop=-1;
     }
+   
 
     public boolean isEmpty()
     {
-        return stacktop==-1;
+        return stacktop<0;
     }
     public boolean isFull()
     {
@@ -20,11 +22,14 @@ public class Stack {
     }
     public boolean push(int value)
     {
-        if(isFull())
+
+        synchronized(lock)
+        {
+            if(isFull())
         {
             return false;
         }
-        stacktop++;
+        ++stacktop;
         try{
             Thread.sleep(1000);
         }
@@ -34,10 +39,15 @@ public class Stack {
         }
         array[stacktop]=value;
         return true;
+        }
+        
     }
 
     public int pop()
-    {
+    {   
+
+
+        synchronized(lock){
         if(isEmpty())
         {
             return Integer.MIN_VALUE;
@@ -54,5 +64,6 @@ public class Stack {
         stacktop--;
         return obj;
     }
+}
 
 }
